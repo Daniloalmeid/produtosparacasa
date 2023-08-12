b60372a0811eee03e0e939cc6e6771a570de544f
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-08-11 23:57:53 -0300

    atu

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../imagens/menu/menu_FILL0_wght400_GRAD0_opsz48.svg"
+    } else {
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "../imagens/menu/close.svg"
+    }
+}
